dcaaf3c82acf2410497fe40a4121d1eca1ad85a4
Ramon Ramon <rramon@hatch-ed.com>
2024-12-28 13:12:30 -0600

    Add method to validate issue and for testing

---
+++ examples/GetDelegationsForUser/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/hatch-ed-com/ri-sdk-go/pkg/rapididentity"
+)
+
+func main() {
+	baseUrl, err := url.Parse(os.Getenv("RI_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	options := rapididentity.Options{
+		HTTPClient:      &http.Client{},
+		BaseUrl:         baseUrl,
+		ServiceIdentity: os.Getenv("RI_KEY"),
+	}
+
+	client := rapididentity.New(options)
+
+	input := rapididentity.GetDelegationsForUserInput{
+		UserId: "08b5f0ec-d56a-4712-ada5-c86074ab11db",
+	}
+
+	output, err := client.GetDelegationsForUser(input)
+	if err != nil {
+		riError, ok := err.(rapididentity.RapidIdentityError)
+		if ok {
+			log.Fatalf("Method: %s, Request URL: %s, Status Code: %d, Message: %s, Reason: %s",
+				riError.Method,
+				riError.ReqUrl,
+				riError.Code,
+				riError.Message,
+				riError.Reason)
+		}
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", output)
+}
---
+++ pkg/rapididentity/GetDelegationsForUser.go
@@ -0,0 +1,117 @@
+package rapididentity
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type GetDelegationsForUserInput struct {
+	UserId string
+}
+
+type GetDelegationsForUserOutput struct {
+	AggregatedDelegation AggregatedDelegation `json:"aggregatedDelegation"`
+}
+
+type AggregatedDelegation struct {
+	Id                 string              `json:"id"`
+	User               User                `json:"user"`
+	HelpdeskQuestions  []string            `json:"helpdeskQuestions"`
+	DelegationProfiles []DelegationProfile `json:"delegationProfiles"`
+}
+
+type User struct {
+	Id            string   `json:"id"`
+	Dn            string   `json:"dn"`
+	Username      string   `json:"username"`
+	FirstName     string   `json:"firstName"`
+	LastName      string   `json:"lastName"`
+	Email         string   `json:"email"`
+	Distinguisher string   `json:"distinguisher"`
+	ImageUrl      string   `json:"imageUrl"`
+	MobileNumbers []string `json:"mobileNumbers"`
+}
+
+type DelegationProfile struct {
+	Delegation Delegation `json:"delegation"`
+	Profile    Profile    `json:"profile"`
+}
+
+type Delegation struct {
+	Id           string                `json:"id"`
+	Name         string                `json:"name"`
+	Type         string                `json:"type"`
+	SourceBaseDN string                `json:"sourceBaseDN"`
+	TargetBaseDN string                `json:"targetBaseDN"`
+	Attributes   []DelegationAttribute `json:"attributes"`
+	LayoutImage  string                `json:"layoutImage"`
+	Layout1      string                `json:"layout1"`
+	Layout2      string                `json:"layout2"`
+	Layout3      string                `json:"layout3"`
+	Actions      []Action              `json:"actions"`
+}
+
+type Profile struct {
+	Id         string             `json:"id"`
+	Attributes []ProfileAttribute `json:"attributes"`
+}
+
+type DelegationAttribute struct {
+	GalItem       GalItem `json:"galItem"`
+	Name          string  `json:"name"`
+	Editable      bool    `json:"editable"`
+	ShowInList    bool    `json:"showInList"`
+	ShowInDetails bool    `json:"showInDetails"`
+	Required      bool    `json:"required"`
+}
+
+type GalItem struct {
+	Id              string `json:"id"`
+	FriendlyName    string `json:"friendlyName"`
+	AllowMultiValue bool   `json:"allowMultiValue"`
+	Type            string `json:"type"`
+}
+
+type Action struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+}
+
+type ProfileAttribute struct {
+	Id     string   `json:"id"`
+	Name   string   `json:"name"`
+	Values []string `json:"values"`
+}
+
+func (c *Client) GetDelegationsForUser(input GetDelegationsForUserInput) (*GetDelegationsForUserOutput, error) {
+	var output GetDelegationsForUserOutput
+
+	url := fmt.Sprintf("%s/profiles/aggregated/for/%s", c.baseEndpoint, input.UserId)
+	req, err := c.GenerateRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	resBody, err := c.ReceiveResponse(res)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(resBody, &output)
+	if err != nil {
+		return nil, RapidIdentityError{
+			Method:  req.Method,
+			ReqUrl:  req.URL,
+			Message: string(resBody),
+			Reason:  err.Error(),
+			Code:    res.StatusCode,
+		}
+	}
+
+	return &output, nil
+}
