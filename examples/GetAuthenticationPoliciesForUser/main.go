047f14f27dd5ccb8762c1d45447d9915dad5b9e2
Ramon Ramon <rramon@hatch-ed.com>
2025-01-12 16:45:58 -0600

    Add example for GetAuthenticationPoliciesForUser method

---
+++ examples/GetAuthenticationPoliciesForUser/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/hatch-ed-com/ri-sdk-go/pkg/rapididentity"
+)
+
+func main() {
+	baseUrl, err := url.Parse(os.Getenv("RI_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	options := rapididentity.Options{
+		HTTPClient:      &http.Client{},
+		BaseUrl:         baseUrl,
+		ServiceIdentity: os.Getenv("RI_KEY"),
+	}
+
+	client, err := rapididentity.New(options)
+	if err != nil {
+		riError, ok := err.(rapididentity.RapidIdentityError)
+		if ok {
+			log.Fatalf("Request URL: %s, Status Code: %d, Message: %s", riError.ReqUrl, riError.Code, riError.Message)
+		}
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	input := rapididentity.GetAuthenticationPoliciesForUserInput{
+		ShowAuthenticationPolicies: true,
+		ShowClaims:                 true,
+		User: rapididentity.GetAuthenticationPoliciesForUserPayload{
+			Username: os.Getenv("RI_USER"),
+		},
+	}
+
+	output, err := client.GetAuthenticationPoliciesForUser(input)
+	if err != nil {
+		riError, ok := err.(rapididentity.RapidIdentityError)
+		if ok {
+			log.Fatalf("Request URL: %s, Status Code: %d, Message: %s", riError.ReqUrl, riError.Code, riError.Message)
+		}
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", output)
+}
