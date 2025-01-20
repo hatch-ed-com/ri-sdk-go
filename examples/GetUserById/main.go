3752c169ede69f7cabbf204cffc6113f8ca59cc9
Ramon Ramon <rramon@hatch-ed.com>
2025-01-20 10:54:02 -0600

    Add example for GetUserById

---
+++ examples/GetUserById/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/hatch-ed-com/ri-sdk-go/pkg/rapididentity"
+)
+
+func main() {
+	baseUrl, err := url.Parse(os.Getenv("RI_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	options := rapididentity.Options{
+		HTTPClient:      &http.Client{},
+		BaseUrl:         baseUrl,
+		ServiceIdentity: os.Getenv("RI_KEY"),
+	}
+
+	client, err := rapididentity.New(options)
+	if err != nil {
+		riError, ok := err.(rapididentity.RapidIdentityError)
+		if ok {
+			log.Fatalf("Request URL: %s, Status Code: %d, Message: %s", riError.ReqUrl, riError.Code, riError.Message)
+		}
+		log.Fatal(err)
+	}
+
+	input := rapididentity.GetUserByIdInput{
+		Id: "08b5f0ec-d56a-4712-ada5-c86074ab11db",
+	}
+
+	ctx := context.Background()
+	output, err := client.GetUserById(ctx, input)
+	if err != nil {
+		riError, ok := err.(rapididentity.RapidIdentityError)
+		if ok {
+			log.Fatalf("Request URL: %s, Status Code: %d, Message: %s", riError.ReqUrl, riError.Code, riError.Message)
+		}
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%+v\n", output)
+}
